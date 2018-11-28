66424927a5faee50d2210f25eae583c5176bca59
dpolianskyi <dmytro_polianskyi@epam.com>
2018-11-28 10:01:11 -0700

    test-commit

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -22,6 +22,7 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
     private final JdbcTemplate jdbcTemplate;
 
     public JdbcTimeEntryRepository(DataSource dataSource) {
+        //TODO: some check
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
